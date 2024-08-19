2217f379ee7ae1f68c3b12ee7a36d7beeae68a81
tleavitt25526 <tleavitt25526@gmail.com>
2024-08-19 19:26:48 -0400

    Trying to mess with filling posts out through seeds

---
+++ public/js/postFiller.js
@@ -0,0 +1,4 @@
+// This script fills the main page with posts. It reads from postData.json.
+console.log("postFiller.js connected.");
+
+const postData = require('/seeds/postData.json'); // also tried ../ to go back
