5e30fe0a49ce7099b7895a6066c68d6782fe0e13
tleavitt25526 <tleavitt25526@gmail.com>
2024-08-27 18:35:24 -0400

    Pushing to

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,29 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'postgres'
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: 'localhost',
+//     dialect: 'postgres'
+//   }
+// );
+let sequelize;
+
+if (process.env.DB_RUL) {
+  sequelize = new Sequelize(process.env.DB_RUL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "postgres",
+    }
+  );
+}
 
 module.exports = sequelize;
