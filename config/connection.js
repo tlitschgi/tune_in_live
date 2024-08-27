efdf86c26137e2918d3f06c171a59b9e532bbd87
tleavitt25526 <tleavitt25526@gmail.com>
2024-08-27 19:12:33 -0400

    panic mode

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,18 @@
-const Sequelize = require("sequelize");
-require("dotenv").config();
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: 'localhost',
-//     dialect: 'postgres'
-//   }
-// );
 let sequelize;
 
-if (process.env.DB_RUL) {
-  sequelize = new Sequelize(process.env.DB_RUL);
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: "localhost",
-      dialect: "postgres",
+      host: 'localhost',
+      dialect: 'postgres'
     }
   );
 }
