4209e1ba16debee053bdf0c60cbdb5c32a42e468
tlitschgi <tlitschgi@att.net>
2024-08-26 20:16:42 -0500

    Added Bookmark

---
+++ models/Bookmark.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//Set up bookmark table
+class Bookmark extends Model {}
+
+Bookmark.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'post',
+        key: 'id',
+      },
+    },
+    bookmark_date: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'bookmark',
+  }
+);
+
+module.exports = Bookmark;
